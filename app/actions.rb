c95f2040ade990a3a6d6c53017c1021e408c4286
Patrick Murphy <pmmurphy19@gmail.com>
2017-06-28 23:34:47 +0000

    Form for signup

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,29 @@ get'/' do
     @posts = Post.order(created_at:  :desc)
     erb(:index)
 end
+
+get '/signup' do # if a user naviagtes to the path "/signup",
+    @user = User.new  #setup empty @user object
+    erb(:signup)   #render "app/views/signup.erb"
+end
+
+post '/signup' do
+    
+    # grab user input values from params
+    email = params[:email]
+    avatar_url = params[:avatar_url]
+    username = params[:username]
+    password = params[:password]
+    
+    #instantiate a User
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+    
+    #if user validations pass and user is saved
+    if @user.save
+        "User #{username} saved!"
+        
+    else
+        erb(:signup)
+    end
+end
+    
