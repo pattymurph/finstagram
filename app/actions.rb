5aeb646310955469701b22111e1dc4c6d41f5208
Patrick Murphy <pmmurphy19@gmail.com>
2017-07-03 23:29:47 +0000

    Login and session

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,8 @@
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
 get'/' do
     @posts = Post.order(created_at:  :desc)
     erb(:index)
@@ -8,6 +13,31 @@ get '/signup' do # if a user naviagtes to the path "/signup",
     erb(:signup)   #render "app/views/signup.erb"
 end
 
+get '/login' do 
+    erb(:login)
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
+end
+
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+    
+    user = User.find_by(username: username)
+    
+    if user && user.password == password 
+        session[:user_id] = user.id
+        redirect to('/')
+       
+    else
+        @error_message = "Login failed."
+        erb(:login)
+    end
+end
+
 post '/signup' do
     
     # grab user input values from params
@@ -16,12 +46,12 @@ post '/signup' do
     username = params[:username]
     password = params[:password]
     
-    #instantiate a User
+    
     @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
     
-    #if user validations pass and user is saved
+   
     if @user.save
-        "User #{username} saved!"
+       redirect to('/login')
         
     else
         erb(:signup)
